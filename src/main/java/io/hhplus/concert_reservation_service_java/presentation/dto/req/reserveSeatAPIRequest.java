1d8d0f600409619c1281911aa480b3058824f4ba
hyeonwoody <hyeonwoody@gmail.com>
2024-07-05 05:53:20 +0900

    TEST : Reservation Mock API 작성

---
+++ src/main/java/io/hhplus/concert_reservation_service_java/presentation/dto/req/reserveSeatAPIRequest.java
@@ -0,0 +1,7 @@
+package io.hhplus.concert_reservation_service_java.presentation.dto.req;
+
+public class reserveSeatAPIRequest {
+  private Long concertId;
+  private Long concertScheduleId;
+  private Long seatId;
+}
