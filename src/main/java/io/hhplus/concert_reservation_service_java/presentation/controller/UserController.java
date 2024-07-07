e98f8b3a4a430db789b06819c61d6c26b8d7908c
hyeonwoody <hyeonwoody@gmail.com>
2024-07-07 16:19:30 +0900

    HOTFIX : 유저 잔액 충전 HTTP method POST->PATCH 수정

--- src/main/java/io/hhplus/concert_reservation_service_java/presentation/controller/UserController.java
+++ src/main/java/io/hhplus/concert_reservation_service_java/presentation/controller/UserController.java
@@ -7,8 +7,10 @@ import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +36,7 @@ public class UserController {
     return ResponseEntity.ok(mockResponse);
   }
 
-  @PostMapping("/{userId}/charge")
+  @PatchMapping("/{userId}/charge")
   public ResponseEntity<ChargePointAPIResponse> chargePoint (@PathVariable long userId){
     ChargePointAPIResponse mockResponse = new ChargePointAPIResponse(
         10000,
