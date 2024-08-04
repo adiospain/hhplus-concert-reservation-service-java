e06e3db4e9da54c26af095d3e0bab66a685207dc
hyeonwoody <hyeonwoody@gmail.com>
2024-08-05 00:51:44 +0900

    TUNE : TokenRedisRepository 리팩토링
    - TOKEN_TTL_MINUTES 1에서 5로 수정
    - 패턴 매칭과 반복자를 용한 AccessKey로 Token 조회
    - 변수이름 key를 element로 수정

--- src/main/java/io/hhplus/concert_reservation_service_java/domain/token/infrastructure/repository/redis/TokenRedisRepositoryImpl.java
+++ src/main/java/io/hhplus/concert_reservation_service_java/domain/token/infrastructure/repository/redis/TokenRedisRepositoryImpl.java
@@ -3,9 +3,11 @@ package io.hhplus.concert_reservation_service_java.domain.token.infrastructure.r
 import io.hhplus.concert_reservation_service_java.domain.token.infrastructure.jpa.Token;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.redisson.api.RMap;
 import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RSetCache;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.protocol.ScoredEntry;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -24,46 +27,51 @@ public class TokenRedisRepositoryImpl implements TokenRedisRepository {
   private static final String ACTIVE_QUEUE_KEY = "active_queue";
   private static final String TOKEN_KEY_PREFIX = "token:";
   private static final int MAX_ACTIVE_USER = 100;
-  private static final int TOKEN_TTL_MINUTES = 1;
+  private static final int TOKEN_TTL_MINUTES = 5;
 
   @Override
   public Token save(Token token) {
-    String key = TOKEN_KEY_PREFIX + token.getUserId() + ":" + token.getAccessKey();
+    String element = TOKEN_KEY_PREFIX + token.getUserId() + ":" + token.getAccessKey();
     RScoredSortedSet<String> queue = redissonClient.getScoredSortedSet(WAIT_QUEUE_KEY);
-    queue.add(System.currentTimeMillis(),key);
-    return Token.create(token.getUserId(), token.getAccessKey(), queue.rank(key)+1);
+    queue.add(System.currentTimeMillis(), element);
+    return Token.create(token.getUserId(), token.getAccessKey(), queue.rank(element)+1);
   }
 
   @Override
   public Optional<Token> getToken(String accessKey) {
-    RScoredSortedSet<String> waitQueue = redissonClient.getScoredSortedSet(WAIT_QUEUE_KEY);
-    RSetCache<String> activeQueue = redissonClient.getSetCache(ACTIVE_QUEUE_KEY);
+    String pattern = "*:" + accessKey;
 
-    for (long i = 0; i < Long.MAX_VALUE; ++i){
-      String key = TOKEN_KEY_PREFIX + i + ":" + accessKey;
-      if (waitQueue.contains(key)){
-        return Optional.of(Token.create(i, accessKey, waitQueue.rank(key)+1));
-      }
-      if (activeQueue.contains(key)){
-        return Optional.of(Token.create(i, accessKey, 0));
-      }
+    RScoredSortedSet<String> waitQueue = redissonClient.getScoredSortedSet(WAIT_QUEUE_KEY);
+    Iterator<String> waitIterator = waitQueue.iterator(pattern);
+    while (waitIterator.hasNext()){
+      String element = waitIterator.next();
+      Token retrievedToken = parseTokenFromKey(element);
+      return Optional.of(Token.create(retrievedToken.getUserId(), accessKey, waitQueue.rank(element)+1));
+    }
+
+    RSetCache<String> activeQueue = redissonClient.getSetCache(ACTIVE_QUEUE_KEY);
+    Iterator<String> activeIterator = activeQueue.iterator(pattern);
+    while (activeIterator.hasNext()){
+      String element = waitIterator.next();
+      Token retrievedToken = parseTokenFromKey(element);
+      return Optional.of(Token.create(retrievedToken.getUserId(), accessKey, 0));
     }
     return Optional.empty();
   }
 
   @Override
   public Optional<Token> getToken(long userId, String accessKey) {
-    String key = TOKEN_KEY_PREFIX + userId + ":" + accessKey;
+    String element = TOKEN_KEY_PREFIX + userId + ":" + accessKey;
     RScoredSortedSet<String> waitQueue = redissonClient.getScoredSortedSet(WAIT_QUEUE_KEY);
     RSetCache<String> activeQueue = redissonClient.getSetCache(ACTIVE_QUEUE_KEY);
 
-    if (activeQueue.contains(key)){
+    if (activeQueue.contains(element)){
       return Optional.of(Token.create(userId, accessKey, 0));
     }
 
-    Double score = waitQueue.getScore(key);
+    Double score = waitQueue.getScore(element);
     if (score != null){
-      int position = waitQueue.rank(key).intValue();
+      int position = waitQueue.rank(element);
       return Optional.of(Token.create(userId, accessKey, position+1));
     }
     return Optional.empty();
