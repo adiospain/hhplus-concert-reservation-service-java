364c989f306182ec90f9a372562ecf36e042667e
adiospain <hyeonwooj1219@gmail.com>
2024-08-11 19:50:57 +0900

    FIX : 406 상태코드 수정
    - 직렬화 이슈 수정

--- src/main/java/io/hhplus/concert_reservation_service_java/presentation/controller/payment/dto/res/CreatePaymentAPIResponse.java
+++ src/main/java/io/hhplus/concert_reservation_service_java/presentation/controller/payment/dto/res/CreatePaymentAPIResponse.java
@@ -3,11 +3,11 @@ package io.hhplus.concert_reservation_service_java.presentation.controller.payme
 import io.hhplus.concert_reservation_service_java.domain.payment.application.model.PaymentDomain;
 import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
-public class CreatePaymentAPIResponse {
-  private long paymentId;
-  private int price;
-  private int pointAfter;
+
+public record CreatePaymentAPIResponse
+  (long paymentId,
+  int price,
+  int pointAfter){
 
   public static CreatePaymentAPIResponse from(PaymentDomain payment) {
     return new CreatePaymentAPIResponse(payment.getId(), payment.getPrice(), payment.getPointAfter());
