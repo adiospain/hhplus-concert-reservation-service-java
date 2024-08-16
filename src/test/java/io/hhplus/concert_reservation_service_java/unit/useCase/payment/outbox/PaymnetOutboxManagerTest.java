c3d29389d541c3f16f47a9e4fce713bb254f0980
adiospain <hyeonwooj1219@gmail.com>
2024-08-16 23:38:16 +0900

    REVIEW : `PaymentOutboxManager` 테스트 코드 작성
    - 아웃박스 로직 테스트

---
+++ src/test/java/io/hhplus/concert_reservation_service_java/unit/useCase/payment/outbox/PaymnetOutboxManagerTest.java
@@ -0,0 +1,87 @@
+package io.hhplus.concert_reservation_service_java.unit.useCase.payment.outbox;
+
+import io.hhplus.concert_reservation_service_java.domain.payment.infrastructure.event.PaymentEvent;
+import io.hhplus.concert_reservation_service_java.domain.payment.infrastructure.outbox.PaymentOutboxManager;
+import io.hhplus.concert_reservation_service_java.domain.payment.infrastructure.outbox.PaymentOutboxManagerImpl;
+import io.hhplus.concert_reservation_service_java.domain.payment.infrastructure.outbox.jpa.PaymentOutbox;
+import io.hhplus.concert_reservation_service_java.domain.payment.infrastructure.outbox.repository.PaymentOutboxRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.*;
+
+public class PaymnetOutboxManagerTest {
+
+  private final PaymentOutboxRepository paymentOutboxRepository = Mockito.mock(PaymentOutboxRepository.class);
+
+  private final PaymentOutboxManager paymentOutboxManager = new PaymentOutboxManagerImpl(paymentOutboxRepository);
+
+  private PaymentEvent paymentEvent;
+  private PaymentOutbox paymentOutbox;
+
+  @BeforeEach
+  void setUp() {
+    String accesskey = UUID.randomUUID().toString();
+
+    paymentEvent = new PaymentEvent(1L, 10, 1L, accesskey);
+    paymentEvent.createOutboxMessage();
+    paymentOutbox = paymentEvent.getPaymentOutbox();
+  }
+
+  @Test
+  @DisplayName("아웃박스 생성 성공")
+  void create_Success() {
+
+    when(paymentOutboxRepository.save(any(PaymentOutbox.class))).thenReturn(paymentOutbox);
+
+    PaymentOutbox result = paymentOutboxManager.create(paymentEvent);
+
+    verify(paymentOutboxRepository).save(paymentEvent.getPaymentOutbox());
+    assertEquals(paymentOutbox, result);
+  }
+
+  @Test
+  @DisplayName("아웃박스 생성 실패")
+  void create_Failure() {
+    doThrow(new RuntimeException("Failed to save outbox"))
+        .when(paymentOutboxRepository).save(any(PaymentOutbox.class));
+
+    assertThatThrownBy(() -> paymentOutboxManager.create(paymentEvent))
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("Failed to save outbox");
+
+    verify(paymentOutboxRepository).save(paymentEvent.getPaymentOutbox());
+  }
+
+
+  @Test
+  @DisplayName("아웃박스 완료 마크 성공")
+  void markComplete_Success() {
+    long outboxId = 1L;
+    paymentOutboxManager.markComplete(outboxId);
+    verify(paymentOutboxRepository).markComplete(outboxId);
+  }
+
+  @Test
+  @DisplayName("아웃박스 완료 마크 실패")
+  void markComplete_Failure() {
+
+    long outboxId = 1L;
+    doThrow(new RuntimeException("아웃박스 완료 마크 실패"))
+        .when(paymentOutboxRepository).markComplete(outboxId);
+
+
+    assertThatThrownBy(() -> paymentOutboxManager.markComplete(outboxId))
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining("아웃박스 완료 마크 실패");
+    verify(paymentOutboxRepository).markComplete(outboxId);
+  }
+}
