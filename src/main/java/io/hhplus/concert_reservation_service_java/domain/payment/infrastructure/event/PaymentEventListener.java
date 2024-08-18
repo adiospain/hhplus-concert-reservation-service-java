233f637dca31597c09dd91d97e1ef2f696352110
adiospain <hyeonwooj1219@gmail.com>
2024-08-19 00:04:32 +0900

    FEAT : `PaymentEventListener` 토큰 만료 메서드 추가

--- src/main/java/io/hhplus/concert_reservation_service_java/domain/payment/infrastructure/event/PaymentEventListener.java
+++ src/main/java/io/hhplus/concert_reservation_service_java/domain/payment/infrastructure/event/PaymentEventListener.java
@@ -1,10 +1,14 @@
 package io.hhplus.concert_reservation_service_java.domain.payment.infrastructure.event;
 
-import io.hhplus.concert_reservation_service_java.domain.common.event.CustomEvent;
+
 import io.hhplus.concert_reservation_service_java.domain.common.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 
 public interface PaymentEventListener extends EventListener {
 
   void createOutbox(PaymentEvent event);
   void sendMessage(PaymentEvent event);
+
+
+  void expireToken(PaymentEvent event);
 }
